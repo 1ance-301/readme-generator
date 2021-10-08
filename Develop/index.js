d463ea1d5cc233980ffc54908f8de6212f8ae97f
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-10-08 17:55:23 -0400

    questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,79 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: input,
+        name: Name,
+        message: 'What is the name of your project?',
+        validate: NAmeInput => {
+            if (NAmeInput) {
+              return true;
+            } else {
+              console.log('Please enter your Project Name!');
+              return false;
+            }
+        }
+    },
+    {
+        type: input,
+        name: Description,
+        message: 'Write a description about your product:',
+        validate: DescriptionInput => {
+            if (DescriptionInput) {
+              return true;
+            } else {
+              console.log('Please enter a description about your project!');
+              return false;
+            }
+        }
+    },
+    {
+        type: input,
+        name: Installation,
+        message: 'How does the user install your project?'
+    },
+    {
+        type: input,
+        name: Usage,
+        message: 'What does the user need to know about using your project?'
+    },
+    {
+        type: input,
+        name: Credit,
+        message: 'List any collaborators and their links:'
+    },
+    {
+        type: input,
+        name: Liscense,
+        message: 'What liscense would you like your project to have?'
+    },
+    {
+        type: confirm,
+        name: confrimFeatures,
+        message: 'Does your project have any features you would like to add?',
+        default: false
+    },
+    {
+        type: input,
+        name: Features,
+        message: 'List your features below:',
+        when: ({ confrimFeatures }) => confrimFeatures
+    },
+    {
+        type: input,
+        name: Contibute,
+        message: 'What does the user need to know about contributing to the project?'
+    },
+    {
+        type: input,
+        name: Test,
+        message: 'How does the user test the project?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
